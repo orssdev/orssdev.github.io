2cef06efaa006cf0bb156225ac5ae16622fb14c8
Oscar <orss4123@gmail.com>
2025-02-25 12:24:38 -0500

    Changes

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const links = document.querySelectorAll("#link");
+
+links.forEach(link => {
+    link.addEventListener("click", e => {
+        let name = e.target.innerHTML.toLowerCase();
+        if(name == "github")
+        {
+            window.location.href = "https://github.com/orss01";
+        }
+        else if(name == "linkedin")
+        {
+            window.location.href = "https://www.linkedin.com/in/orss01";
+        }
+    });
+});
