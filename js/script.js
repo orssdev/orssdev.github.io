a4db0f222dc4b0c280b5523b8de5683e7fbf7ae5
Oscar <orss4123@gmail.com>
2025-02-25 13:34:18 -0500

    proto design 1

--- js/script.js
+++ js/script.js
@@ -3,15 +3,15 @@ const links = document.querySelectorAll("#link");
 links.forEach(link => {
     link.addEventListener("click", e => {
         let name = e.target.innerHTML.toLowerCase();
-        if(name == "github")
+        if(name.includes("github"))
         {
             window.location.href = "https://github.com/orss01";
         }
-        else if(name == "linkedin")
+        else if(name.includes("linkedin"))
         {
             window.location.href = "https://www.linkedin.com/in/orss01";
         }
-        else if(name == "resume")
+        else if(name.includes("resume"))
         {
             window.location.href = "https://orss01.dev/assets/pdf/resume.pdf";
         }
